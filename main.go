92b5df68df250560e50d7d9afa5561f2f9db6bb1
hanbeul <han310@gmail.com>
2018-03-14 23:03:56 -0400

    Added post request handler

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
+var arr []string = []string{"string1"}
+
 func handle(w http.ResponseWriter, r *http.Request) {
-	var arr []string
-	arr = make([]string, 0)
-	arr = append(arr, "String0")
-	arr = append(arr, "String1")
-	arr = append(arr, "String2")
-	var output, _ = json.Marshal(arr)
-	//fmt.Println(output)
-	//fmt.Fprintf(w, string(output))
-	w.Write(output)
+	switch r.Method {
+	case "GET":
+		var output, _ = json.Marshal(arr)
+		//fmt.Println(output)
+		//fmt.Fprintf(w, string(output))
+		w.Write(output)
+	case "POST":
+		var data, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request Body", http.StatusInternalServerError)
+		}
+		arr = append(arr, string(data))
+	}
 }
 
 func main() {
@@ -24,6 +31,6 @@ func main() {
 
 	http.HandleFunc("/", handle)
 
-	fmt.Println("Server started on port 8080...")
-	fmt.Println(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started on port 9001...")
+	fmt.Println(http.ListenAndServe(":9001", nil))
 }
